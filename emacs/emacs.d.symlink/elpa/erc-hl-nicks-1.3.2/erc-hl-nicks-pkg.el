d324471570d4d6a0d75a3f48579d0da00142db54
Donald Hunter <donaldh@cisco.com>
2017-11-27 10:31:23 +0000

    Remove erc-hl-nicks mode

--- emacs/emacs.d.symlink/elpa/erc-hl-nicks-1.3.2/erc-hl-nicks-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "erc-hl-nicks" "1.3.2" "ERC nick highlighter that ignores uniquifying chars when colorizing" 'nil :url "http://www.github.com/leathekd/erc-hl-nicks")
