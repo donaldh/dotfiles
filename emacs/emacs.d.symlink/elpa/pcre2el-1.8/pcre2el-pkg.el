2c6e4ceed2969ef3dd11a699875236ca83090c96
Donald Hunter <donaldh@cisco.com>
2017-11-27 10:29:08 +0000

    Add visual-regexp-steroids for PCRE regepxs

---
+++ emacs/emacs.d.symlink/elpa/pcre2el-1.8/pcre2el-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "pcre2el" "1.8" "regexp syntax converter" '((emacs "24") (cl-lib "0.3")) :commit "166a10472002010692dbc35f323ffb8110a294c5" :url "https://github.com/joddie/pcre2el")
