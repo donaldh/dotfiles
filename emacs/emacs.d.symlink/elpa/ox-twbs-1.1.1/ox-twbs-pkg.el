6805ce12baaf89bbe3c736886e3e900afe1aaa92
Donald Hunter <donaldh@cisco.com>
2018-03-12 10:27:35 +0000

    Add org babel mode features

---
+++ emacs/emacs.d.symlink/elpa/ox-twbs-1.1.1/ox-twbs-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ox-twbs" "1.1.1" "Bootstrap compatible HTML Back-End for Org" 'nil :commit "2414e6b1de7deb6dd2ae79a7be633fdccb9c2f28" :url "https://github.com/marsmining/ox-twbs" :keywords '("org" "html" "publish" "twitter" "bootstrap"))
