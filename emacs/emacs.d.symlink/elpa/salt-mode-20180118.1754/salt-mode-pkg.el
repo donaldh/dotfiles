84796ce49802c6e8cc3889a409b7abaf7cb541ff
Donald Hunter <donaldh@cisco.com>
2018-07-11 14:49:01 +0100

    ack ensime scala salt

---
+++ emacs/emacs.d.symlink/elpa/salt-mode-20180118.1754/salt-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "salt-mode" "20180118.1754" "Major mode for Salt States" '((emacs "24.4") (yaml-mode "0.0.12") (mmm-mode "0.5.4") (mmm-jinja2 "0.1")) :commit "e46c28ef77663391519646c79641c9d177f70d35" :keywords '("languages") :url "https://github.com/glynnforrest/salt-mode")
