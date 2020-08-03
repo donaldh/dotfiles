b85136989b54882c318981c0aff7ad9bc347149d
Donald Hunter <donaldh@cisco.com>
2020-08-03 12:08:41 +0100

    Add eclipse aliases for Java / RCP

--- eclipse/aliases.zsh
+++ eclipse/aliases.zsh
@@ -1,1 +1,2 @@
-alias eclipsews='open -n -a /Applications/Eclipse\ RCP.app --args -data'
+alias eclipsews='open -n -a /Applications/Eclipse\ Java.app --args -data'
+alias eclipsewsrcp='open -n -a /Applications/Eclipse\ RCP.app --args -data'
