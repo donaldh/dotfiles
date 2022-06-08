5c4ae105442c346c8baaa825f67432ad212495a7
Donald Hunter <donald.hunter@redhat.com>
2022-06-08 20:01:50 +0100

    elpa updates

---
+++ emacs/emacs.d.symlink/elpa/dash-20220606.2204/dash-pkg.el
@@ -0,0 +1,12 @@
+(define-package "dash" "20220606.2204" "A modern list library for Emacs"
+  '((emacs "24"))
+  :commit "ae0ce7959e647c2a5a9553fd4b8dee844e2fa357" :authors
+  '(("Magnar Sveen" . "magnars@gmail.com"))
+  :maintainer
+  '("Magnar Sveen" . "magnars@gmail.com")
+  :keywords
+  '("extensions" "lisp")
+  :url "https://github.com/magnars/dash.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/emacs.d.symlink/elpa/magit-section-20220513.1429/magit-section-pkg.el
@@ -0,0 +1,14 @@
+(define-package "magit-section" "20220513.1429" "Sections for read-only buffers."
+  '((emacs "25.1")
+    (compat "28.1.1.0")
+    (dash "20210826"))
+  :commit "9b48dd7e3618ac3736f66ef964ae5e1fedd54f98" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("tools")
+  :url "https://github.com/magit/magit")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
