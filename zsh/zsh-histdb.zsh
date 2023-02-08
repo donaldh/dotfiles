135812769131f2dc49249749c7961168be4a1204
Donald Hunter <donald.hunter@gmail.com>
2023-02-08 13:48:37 +0000

    Enable zsh-histdb interactive search with ^r
    Also disable macOS per-shell session history

--- zsh/zsh-histdb.zsh
+++ zsh/zsh-histdb.zsh
@@ -1,3 +1,5 @@
 HISTDB_TABULATE_CMD=(sed -e $'s/\x1f/\t/g')
 source $HOME/git/zsh-histdb/sqlite-history.zsh
+source $HOME/git/zsh-histdb/histdb-interactive.zsh
 autoload -Uz add-zsh-hook
+bindkey '^r' _histdb-isearch
