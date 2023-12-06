dbd773dbffaf0a364916cfcea3ce5932a140481b
Donald Hunter <donald.hunter@gmail.com>
2023-12-06 14:51:58 +0000

    Add helm to emacs
    Signed-off-by: Donald Hunter <donald.hunter@gmail.com>

---
+++ emacs/emacs.d.symlink/elpa/helm-20230625.556/helm-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm" "20230625.556" "Helm is an Emacs incremental and narrowing framework"
+  '((helm-core "3.9.0")
+    (popup "0.5.3"))
+  :commit "4c3abcd8eaa4a3f7a636a2dc6cf842585ecf1553" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ emacs/emacs.d.symlink/elpa/helm-core-20230625.556/helm-core-pkg.el
@@ -0,0 +1,13 @@
+(define-package "helm-core" "20230625.556" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.7"))
+  :commit "4c3abcd8eaa4a3f7a636a2dc6cf842585ecf1553" :authors
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainers
+  '(("Thierry Volpiatto" . "thievol@posteo.net"))
+  :maintainer
+  '("Thierry Volpiatto" . "thievol@posteo.net")
+  :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
